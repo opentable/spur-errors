f3099e2420a9a79a7e14ae267dda46ac7de19f8f
Agustin Colchado <agustin@colchado.comm>
2015-10-28 21:32:00 -0700

    Cleaning whitespaces

--- test/globals.js
+++ test/globals.js
@@ -1,8 +1,8 @@
-var path              = require("path");
-global.chai           = require('chai');
-global.expect         = chai.expect
-global.srcDir         = path.resolve(__dirname, "../src");
-global.rootDir        = path.resolve(__dirname, "../");
+var path             = require("path");
+global.chai          = require('chai');
+global.expect        = chai.expect;
+global.srcDir        = path.resolve(__dirname, "../src");
+global.rootDir       = path.resolve(__dirname, "../");
 
 process.env.NODE_ENV = "test";
 
