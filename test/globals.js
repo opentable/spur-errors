2a543f8735ff916123464d2fc013b2fb7a39553b
Agustin Colchado <agustin@colchado.com>
2016-06-15 11:37:42 -0700

    Test Configuration

--- test/globals.js
+++ test/globals.js
@@ -1,9 +1,10 @@
-var path             = require("path");
-global.chai          = require('chai');
-global.expect        = chai.expect;
-global.srcDir        = path.resolve(__dirname, "../src");
-global.rootDir       = path.resolve(__dirname, "../");
+const path = require('path');
+const chai = require('chai');
+global.chai = chai;
+global.expect = chai.expect;
+global.srcDir = path.resolve(__dirname, '../src');
+global.rootDir = path.resolve(__dirname, '../');
 
-process.env.NODE_ENV = "test";
+process.env.NODE_ENV = 'test';
 
 process.setMaxListeners(1000);
