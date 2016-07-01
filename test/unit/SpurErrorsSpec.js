52ebbde99c1597494e61d230cb2874222546127f
Timmy Willison <timmywillisn@gmail.com>
2016-07-01 18:40:30 -0400

    Convert class syntax to more succinct prototypes
    - Also fixes tests

--- test/unit/SpurErrorsSpec.js
+++ test/unit/SpurErrorsSpec.js
@@ -5,8 +5,6 @@ describe('SpurErrors;', () => {
   it('test error', () => {
     const notFound1 = SpurErrors.NotFoundError.create('could not find it');
     const notFound2 = SpurErrors.NotFoundError.create('where is it?', notFound1);
-    console.log('getting past create');
-
     const e = SpurErrors.BaseError.create('leaf error', notFound2)
       .setErrorCode('leaf_error')
       .stack;
@@ -14,12 +12,12 @@ describe('SpurErrors;', () => {
     console.log(e);
   });
 
-  xit('test callee', () => {
+  it('test callee', () => {
     const callee = Callee;
     console.error(callee.run().stack);
   });
 
-  xit('errorByStatusCode()', () => {
+  it('errorByStatusCode()', () => {
     expect(SpurErrors.errorByStatusCode(999)).to.equal(undefined);
     expect(SpurErrors.errorByStatusCode(503))
       .to.equal(SpurErrors.ServiceUnavailableError);
